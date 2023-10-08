34de676814a212e9f03485abb3256bcd278d05cd
rnascimento13 <rnascimento13@hotmail.com>
2023-10-08 04:05:44 -0300

    redirect instead middleware

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth/next";
+
 import Navbar from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
 import { checkSubscription } from "@/lib/subscription";
@@ -10,6 +13,13 @@ const DashboardLayout = async ({
 }) => {
   const apiLimitCount = await getApiLimitCount();
   const isPro = await checkSubscription();
+
+  const session = await getServerSession();
+  if (!session || !session.user) {
+    redirect("/api/auth/signin?callbackUrl=/dashboard");
+  }
+
+
   return ( 
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
