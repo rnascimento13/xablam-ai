05a724e605f8a1b55432b4929c36d2af89f4c050
rnascimento13 <rnascimento13@hotmail.com>
2023-10-08 01:31:20 -0300

    middleware

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,3 @@
-import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth/next";
-
 import Navbar from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
 import { checkSubscription } from "@/lib/subscription";
@@ -13,18 +10,13 @@ const DashboardLayout = async ({
 }) => {
   const apiLimitCount = await getApiLimitCount();
   const isPro = await checkSubscription();
-  const session = await getServerSession();
-
-  if (!session || !session.user) {
-    redirect("/api/auth/signin?callback=/dashboard");
-  }
   return ( 
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
         <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       </div>
       <main className="md:pl-72 pb-10">
-        <Navbar />
+        <Navbar isPro={isPro} apiLimitCount={apiLimitCount} />
         {children}
       </main>
     </div>
