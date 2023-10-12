34e22a0cd9f69c19d829244e34df21986bb57e7d
rnascimento13 <rnascimento13@hotmail.com>
2023-10-12 04:06:34 -0300

    image webhook clean

--- app/api/image/webhook/route.ts
+++ app/api/image/webhook/route.ts
@@ -1,27 +1,15 @@
-import { headers } from "next/headers"
 import { NextResponse } from "next/server"
-import { getServerSession } from "next-auth/next"
-
-import { mountUserId } from "@/lib/utils"
-import { authOptions } from "@/lib/auth-options"
 import { pusher } from "@/lib/pusher";
 
 export async function POST(req: Request) {
 
   const data = await req.json()
-
-  // const delay = data.delayTime;
+  // console.log('image/webhook in: ', data)
   const imgId = data.id;
-  // const imgsData = await data.output.images;
-  // const images = imgsData.map((e: string) => `data:image/gif;base64,${e}`)
-  const res = { imgId }
-  console.log('webhook ok: ', res)
+  const status = data.status;
+  const res = { imgId, status }
+  // console.log('image/webhook out: ', res)
     // Pusher notification
-    const response = await pusher.trigger("chat", "chat-event", res);
-    // console.log('webhook ok: ', response)
-    
-
-    // console.log('runpod webhook POST body: ',body)
-    // return NextResponse.json({ message: "This Worked", success: true });
+  const response = await pusher.trigger("chat", imgId, res);
     return NextResponse.json( "Ok", { status: 200 });
 };
